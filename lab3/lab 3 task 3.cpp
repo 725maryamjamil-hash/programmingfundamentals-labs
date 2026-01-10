09bfe347884b1cb57a1243e9653171263efe9d93
725maryamjamil-hash <725maryamjamil@gmail.com>
2026-01-10 09:43:31 -0800

    lab 1

--- lab3/lab 3 task 3.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<iomanip>
-using namespace std;
-int main(){
-const double score1=85.5,score2=90.75,score3=88.25;
-const double average=(score1+score2+score3)/3;
-cout<<"The average of 85.5 , 90.75 and 88.25 = "<<fixed<<setprecision(2)<<average<<endl;
-return(0);
-}
