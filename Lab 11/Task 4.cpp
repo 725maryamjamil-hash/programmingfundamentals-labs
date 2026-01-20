13b4c0d86f32e74ac3fe2ad28870dbab76278007
Maryam Jamil <251764272+725maryamjamil-hash@users.noreply.github.com>
2026-01-20 06:56:26 -0800

    Delete Lab 11 directory

--- Lab 11/Task 4.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-	int arr[] = {1,2,3,4,5};
-	int length = sizeof(arr)/sizeof(arr[0]);
-	cout << "Array length : " << length << endl;
-	return (0);
-}
